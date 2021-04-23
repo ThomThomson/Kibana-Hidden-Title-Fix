12248845fbc1f4397df07d100618c77469699b67
Devon A Thomson <devon.thomson@hotmail.com>
2021-04-23 17:08:21 -0400

    Created command line tool

---
+++ index.js
@@ -0,0 +1,79 @@
+const prompt = require("prompt-async");
+const { Client } = require('@elastic/elasticsearch')
+
+const promptSchema = {
+  properties: {
+    esUrl: {
+      required: true,
+      message: 'Elasticsearch endpoint',
+    },
+    username: {
+      pattern: /^[a-zA-Z\s\-]+$/,
+      required: true
+    },
+    password: {
+      hidden: true,
+      required: true
+    }
+  }
+};
+
+const updatePanelsJson = (originalJson) => {
+  let updateCount = 0;
+  const panelsParsed = JSON.parse(originalJson);
+  panelsParsed?.forEach((panel) => {
+    if(panel.title === '' && panel.embeddableConfig?.hidePanelTitles === false) {
+      panel.embeddableConfig.hidePanelTitles = true;
+      updateCount++;
+    }
+  });
+  return { newPanelsJSON: JSON.stringify(panelsParsed), updateCount };
+};
+
+(async () => {
+  prompt.start();
+  const { esUrl, username, password } = await prompt.get(promptSchema);
+  const kibanaIndex = '.kibana';
+  const client = new Client({
+    node: esUrl,
+    auth: {
+      username: username,
+      password: password
+    }
+  });
+
+  const allDashboards = await client.search({
+    index: '.kibana',
+    body: {
+      "query": {
+        "match": {
+          "type": "dashboard"
+        }
+      }
+    }
+  });
+  allDashboards.body.hits?.hits?.forEach(async (hit) => {
+    const dashboardId = hit._id;
+    const dashboard = hit._source.dashboard;
+    console.log('Analyzing: ', dashboard.title);
+    const { newPanelsJSON, updateCount } = updatePanelsJson(dashboard.panelsJSON);
+
+    if (updateCount === 0) {
+      console.log('--No panels to update\n');
+      return;
+    }
+
+    await client.update({
+      index: kibanaIndex,
+      id: dashboardId,
+      body: {
+        doc: {
+          dashboard: {
+            panelsJSON: newPanelsJSON
+          }
+        }
+      }
+    });
+    console.log(`--updated ${updateCount} panels\n`);
+    });
+})();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Kibana Hidden Title Fix
+
+### Issue
+In Kibana 7.10, a backwards compatibility bug was introduced to dashboards. Pre 7.10, the 'hidden' setting for dashboard titles was stored by setting the title to a blank string. In 7.10, this functionality was changed to be more explicit by using the hidePanelTitles key. Unfortunately in 7.10 and 7.11 any panel which was hidden on previous versions would be shown. In version 7.12.1, this BWC bug was fixed, but some clusters cannot update to that version.
+
+### Workaround
+This small node program is a workaround for that issue. It takes elasticsearch credentials, and sets the hidePanelTitles key to true for any panel on any dashboard where the title is set to a blank string. This should have the effect of hiding every title which was configured to be hidden beforehand.
+
+### Known Limitations
+This will not work on any dashboards which have been saved
+This currently works with kibana 7.11.2, but you can easily change that by installing a different version of the ES Client
+
+### How to Run
+`yarn install`
+`node index.js`
